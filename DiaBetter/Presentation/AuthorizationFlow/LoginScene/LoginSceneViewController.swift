48562aedcfbea48a7c2c73fae6f8187cc07ae517
Krm1v <vladkarmalov@gmail.com>
2023-08-09 15:47:56 +0300

    - Changed selectors to publishers on login scene

--- DiaBetter/Presentation/AuthorizationFlow/LoginScene/LoginSceneViewController.swift
+++ DiaBetter/Presentation/AuthorizationFlow/LoginScene/LoginSceneViewController.swift
@@ -29,8 +29,10 @@ final class LoginSceneViewController: BaseViewController<LoginSceneViewModel> {
 	
 	override func viewWillAppear(_ animated: Bool) {
 		super.viewWillAppear(animated)
-		setupNavBar()
-		playBackgroundVideo()
+		DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) { [weak self] in
+			guard let self = self else { return }
+			self.playBackgroundVideo()
+		}
 	}
 	
 	//MARK: - Deinit
@@ -41,11 +43,6 @@ final class LoginSceneViewController: BaseViewController<LoginSceneViewModel> {
 
 //MARK: - Private extension
 private extension LoginSceneViewController {
-	//MARK: - SetupUI
-	func setupNavBar() {
-		navigationController?.navigationBar.isHidden = true
-	}
-	
 	//MARK: - AVPlayer
 	func playBackgroundVideo() {
 		guard let path = Bundle.main.path(forResource: Files.loginBackgroundMp4.name,
@@ -55,7 +52,9 @@ private extension LoginSceneViewController {
 		let asset = AVAsset(url: URL(fileURLWithPath: path))
 		let item = AVPlayerItem(asset: asset)
 		self.player = AVQueuePlayer(playerItem: item)
-		self.playerLooper = AVPlayerLooper(player: player!, templateItem: item)
+		if let player = self.player {
+			self.playerLooper = AVPlayerLooper(player: player, templateItem: item)
+		}
 		let playerLayer = AVPlayerLayer(player: player)
 		playerLayer.frame = self.view.bounds
 		playerLayer.videoGravity = .resizeAspectFill
@@ -65,34 +64,41 @@ private extension LoginSceneViewController {
 	
 	//MARK: - NotificationCenter
 	func addObservers() {
-		notificationCenter.addObserver(self,
-									   selector: #selector(willResignActive),
-									   name: UIApplication.willResignActiveNotification,
-									   object: nil)
-		notificationCenter.addObserver(self,
-									   selector: #selector(didBecomeActive),
-									   name: UIApplication.didBecomeActiveNotification,
-									   object: nil)
+		notificationCenter.publisher(for: UIApplication.willResignActiveNotification,
+									 object: nil)
+		.sink { [weak self] _ in
+			guard let self = self else { return }
+			self.willResignActive()
+		}
+		.store(in: &cancellables)
+		
+		notificationCenter.publisher(for: UIApplication.didBecomeActiveNotification,
+									 object: nil)
+		.sink { [weak self] _ in
+			guard let self = self else { return }
+			self.didBecomeActive()
+		}
+		.store(in: &cancellables)
 	}
 	
 	func removeObservers() {
 		notificationCenter.removeObserver(self,
 										  name: UIApplication.willResignActiveNotification,
-										  object: nil)
+										  object: nil
+		)
+		
 		notificationCenter.removeObserver(self,
 										  name: UIApplication.didBecomeActiveNotification,
-										  object: nil)
+										  object: nil
+		)
 	}
 	
-	//MARK: - @Objc methods
-	@objc
 	func willResignActive() {
 		if let player = player, player.isPlaying {
 			player.pause()
 		}
 	}
 	
-	@objc
 	func didBecomeActive() {
 		if let player = player, !player.isPlaying {
 			player.play()
